521e1800b8550b191f533dd6cb585238a170b061
Vladislav Shchukin <dorry@DorryBook.local>
2022-04-19 18:14:52 +0300

    first version without storyboard

---
+++ Rick and Morty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rick and Morty
+//
+//  Created by Vladislav Shchukin on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.yellow
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
